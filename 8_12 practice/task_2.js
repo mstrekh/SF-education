c77f728db76e717e3f74385853222a7ae54c4ac8
mstrekh <mstrekh@mail.ru>
2021-05-20 14:42:12 +0300

    Update task_2.js

--- 8_12 practice/task_2.js
+++ 8_12 practice/task_2.js
@@ -3,41 +3,18 @@
 //Написать программу, которая в зависимости от типа данных Х выводит в консоль сообщение вида: «X — число».
 //Опишите три случая: когда х = числу, строке или логическому типу. В других случаях выводите сообщение: «Тип x не определён».
 
-let varX;
-
-switch (typeof(varX)) {
-	case 'number': 
-     if (isNaN(varX) == false) {
-		console.log('X - число');
-		}
-		break;
-
-	case 'string': 
-		console.log('X - Строка');
-		break;
-
-	case 'boolean': 
-		console.log('Логический тип');
-		break;
-
-	default:
-		console.log('Тип х не определён');
-}
-
-
-
 let varX = prompt('Введите значение X');
 
 if (varX =='false' || varX == 'true') {
 	console.log('X - логический тип');
 }
 else {
-	if(varX == 'null' || varX == 'undefined' || isNan(parseInt(varX)) == true) {
+	if(varX == 'null' || varX == 'undefined') {
 		console.log('Тип X не определён');
 	}
 	else {
 		console.log()
-		if(typeof(parseInt(varX)) == 'number') {
+		if(typeof(parseInt(varX)) == 'number' && isNaN(varX) == false) {
 			console.log('X - число');
 		}
 		else {
