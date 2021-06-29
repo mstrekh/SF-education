05f38c0cdb6bb3f9969d7389936107a66659c989
mstrekh <mstrekh@mail.ru>
2021-06-29 23:02:21 +0300

    Updated task 4

--- 9_7_practice/task3/script.js
+++ 9_7_practice/task3/script.js
@@ -1,22 +1,47 @@
-const trafficLightEl = document.querySelector('#trafficLight');
+const trafficLightEl = document.querySelector('#trafficLight_1');
+const trafficLightE2 = document.querySelector('#trafficLight_2');
+const trafficLightE3 = document.querySelector('#trafficLight_3');
 
 function makeGreen() {
     trafficLightEl.style.background = ('green');
-    trafficLightEl.removeEventListener('click', makeGreen);
-    trafficLightEl.addEventListener('click', makeYellow);
+    trafficLightE3.style.background = ('black');
 }
 
 function makeYellow() {
-    trafficLightEl.style.background = ('yellow');
-    trafficLightEl.removeEventListener('click', makeYellow);
-    trafficLightEl.addEventListener('click', makeRed);
+    trafficLightE2.style.background = ('yellow');
+    trafficLightEl.style.background = ('black');
 }
 
 function makeRed() {
-    trafficLightEl.style.background = ('red');
-    trafficLightEl.removeEventListener('click', makeRed);
-    trafficLightEl.addEventListener('click', makeGreen);
+    trafficLightE3.style.background = ('red');
+    trafficLightE2.style.background = ('black');
 }
 
-trafficLightEl.addEventListener('click', makeGreen);
+trafficLightEl.addEventListener('click', changeColor);
+trafficLightE2.addEventListener('click', changeColor);
+trafficLightE3.addEventListener('click', changeColor);
 
+let counter = 1;
+function changeColor() {
+   switch(counter) {
+       case 1 : {
+           makeGreen();
+            counter++;
+        }
+        break;
+
+        case 2 : {
+            makeYellow();
+            counter++;
+        }
+        break;
+        
+        case 3 : {
+            makeRed();
+            counter = 1;
+        }
+        break;
+    }
+}
+    
+    
